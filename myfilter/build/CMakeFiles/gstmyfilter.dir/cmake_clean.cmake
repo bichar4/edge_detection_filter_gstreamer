9b9ec6edd47e5065bf63ab597c1f6739e64052f8
bichar4 <bichar4@gmail.com>
2020-06-09 21:27:28 +0545

    first commit

---
+++ myfilter/build/CMakeFiles/gstmyfilter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gstmyfilter.dir/gstmyfilter.cpp.o"
+  "libgstmyfilter.pdb"
+  "libgstmyfilter.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gstmyfilter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
